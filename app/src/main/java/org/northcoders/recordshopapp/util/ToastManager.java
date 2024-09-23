d863c72f35d65cdf415b60fc576c5a349232ec5a
unknown <chatonode@gmail.com>
2024-09-23 22:26:36 +0100

    feat: toast message for searching

---
+++ app/src/main/java/org/northcoders/recordshopapp/util/ToastManager.java
@@ -0,0 +1,18 @@
+package org.northcoders.recordshopapp.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastManager {
+    private static Toast toast;
+
+
+    public static void showToast(Context context, String message) {
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
