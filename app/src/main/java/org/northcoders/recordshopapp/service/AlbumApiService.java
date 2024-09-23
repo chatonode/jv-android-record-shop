1b5a78a40793023142582a780f405471458e1302
unknown <chatonode@gmail.com>
2024-09-23 10:08:46 +0100

    feat: method signature of album api interface

--- app/src/main/java/org/northcoders/recordshopapp/service/AlbumApiService.java
+++ app/src/main/java/org/northcoders/recordshopapp/service/AlbumApiService.java
@@ -8,11 +8,21 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface AlbumApiService {
     @GET("api/v1/album")
     Call<GetAlbumsResponse> getAllAlbums();
 
+    @GET("api/v1/album")
+    Call<GetAlbumsResponse> getFilteredAlbums(
+            @Query("title") String title,
+            @Query("year") Integer year
+//            @Query("genre") String genre,
+//            @Query("format") String format
+    );
+
     @POST("api/v1/album")
     Call<NewAlbumResponse> postAlbum(@Body NewAlbumRequestBody albumRequestBody);
+
 }
